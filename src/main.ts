4afb0cd0dc8d276537fe1b163ec0e33f2e2f0e98
Darek Dziamalek <ddziamalek@gmail.com>
2016-07-04 01:50:04 -0400

    crud

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,9 @@ import 'zone.js/dist/zone';
 import 'rxjs/Rx';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app/app.component.ts';
+import {enableProdMode} from "@angular/core";
 
+if (app.environment === 'production') {
+    enableProdMode();
+}
 bootstrap(AppComponent);
